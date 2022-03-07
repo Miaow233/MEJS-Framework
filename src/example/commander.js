e596b583e561ec9a35f701181e1bdb9ac815ae78
Miaow233 <miaow233@qq.com>
2022-03-08 00:04:54 +0800

    移除不必要示例

--- src/example/commander.js
+++
@@ -1,5 +0,0 @@
-// 命令解析
-import { cli } from './utils/commander.js' // 引入命令解析
-let msg = 'hello world' // 假设你收到这样一条消息
-let argv = cli(msg) // 解析命令
-console.log(JSON.stringify(argv, null, '\n')) // 输出解析结果
